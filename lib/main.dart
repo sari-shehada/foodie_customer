8526a89502598f1fd0be2ea35510d7459d527683
Sari Shehada <sarishehada@gmail.com>
2024-01-27 16:30:32 +0300

    Added packages and services

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:foodie/core/services/shared_prefs_service.dart';
 import 'package:foodie/pages/loader_page/loader_page.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SharedPreferencesService.init();
+  await ScreenUtil.ensureScreenSize();
+  runApp(const Application());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Application extends StatelessWidget {
+  const Application({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Foodie',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
-        useMaterial3: true,
-      ),
-      home: const LoaderPage(),
+    return ScreenUtilInit(
+      splitScreenMode: false,
+      useInheritedMediaQuery: true,
+      designSize: const Size(390, 844),
+      builder: (context, child) {
+        return MaterialApp(
+          title: 'Foodie',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+            useMaterial3: true,
+          ),
+          home: const LoaderPage(),
+        );
+      },
     );
   }
 }
--- lib/pages/loader_page/loader_page.dart
+++ lib/pages/loader_page/loader_page.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LoaderPage extends StatelessWidget {
   const LoaderPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SizedBox.expand(
+      child: Column(
+        children: [
+          const Text('Foodie'),
+          SizedBox(
+            height: 40.h,
+          )
+        ],
+      ),
+    );
   }
 }
